09560823ac09d2427aa49d6299fa74d3746bafd5
gergelykovacs89 <gergelyk89@gmail.com>
2019-03-18 15:26:04 +0100

    add AuthorDetail wip author showing up, correct routing, params id change picked up

--- src/components/authors/AuthorDetail.js
+++ src/components/authors/AuthorDetail.js
@@ -5,18 +5,72 @@ import {
   Paper,
   Typography,
   CircularProgress,
-  withStyles
+  withStyles,
+  Grid,
+  Button,
+  Avatar
 } from "@material-ui/core";
 
 const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
   paper: {
-    marginTop: "2vh"
+    padding: theme.spacing.unit * 2,
+    marginTop: "1vw",
+    [theme.breakpoints.up("sm")]: {
+      marginTop: "1vw",
+      margin: "auto",
+      width: "70vw"
+    }
+  },
+  avatar: {
+    marginLeft: "3vw",
+    [theme.breakpoints.up("md")]: {
+      marginLeft: "7vw"
+    },
+    marginTop: "5vh",
+    minHeight: "100px",
+    minWidth: "100px",
+    height: "13vw",
+    width: "13vw"
+  },
+  authorName: {
+    marginTop: "4vh",
+    fontSize: "20px",
+    [theme.breakpoints.up("sm")]: {
+      display: "inline-block"
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2vw"
+    }
+  },
+  authorDescription: {
+    fontSize:"1vw"
+  },
+  followButton: {
+    marginTop: "1vw",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: "2vw",
+      marginTop: "0"
+    }
+  },
+  authorControl: {
+    paddingLeft: "2vw",
+    paddingRight: "2vw",
+    marginTop: "3vw"
   }
 });
 
 class AuthorDetail extends React.Component {
   componentDidMount() {
     this.props.getAuthor(this.props.match.params.id);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.match.params.id !== this.props.match.params.id) {
+      this.props.getAuthor(nextProps.match.params.id);
+    }
   }
 
   circularProgress = (
@@ -31,13 +85,49 @@ class AuthorDetail extends React.Component {
     }
     const { author, classes } = this.props;
     return (
-      <Fragment>
+      <div className={classes.root}>
         <Paper className={classes.paper}>
-          <Typography variant="h5" component="h3">
-            {author.name}
-          </Typography>
+          <Grid
+            justify="space-between"
+            container
+            spacing={24}
+            alignItems="flex-start"
+          >
+            <Grid item xs={4}>
+              <Avatar
+                alt={author.name}
+                src={author.imgPath}
+                className={classes.avatar}
+              />
+            </Grid>
+            <Grid item xs={8} className={classes.authorControl}>
+              <Typography variant="h1" className={classes.authorName}>
+                {author.name}
+              </Typography>
+              <span>
+                <Button variant="contained" size="small" className={classes.followButton}>
+                  Follow
+                </Button>
+                <Button variant="outlined" size="small" className={classes.followButton}>
+                  Unfollow
+                </Button>
+              </span>
+              <Typography variant="subtitle1" className={classes.authorData}>
+                {"0"} stories {author.followers.length} followers{" "}
+                {author.following.length} following
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                className={classes.authorDescription}
+              >
+                {author.description}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} />
+            <Grid item xs={6} />
+          </Grid>
         </Paper>
-      </Fragment>
+      </div>
     );
   }
 }
