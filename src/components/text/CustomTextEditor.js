fd2975a3d47cfa4ab3edf755c162ef366f9b4eb5
gergelykovacs89 <gergelyk89@gmail.com>
2019-03-20 17:37:12 +0100

    add CreateStory form rich-text input (playground version included)

---
+++ src/components/text/CustomTextEditor.js
@@ -0,0 +1,113 @@
+import React, { Fragment } from "react";
+import Editor from "draft-js-plugins-editor";
+
+import createToolbarPlugin, { Separator } from "draft-js-static-toolbar-plugin";
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton
+} from "draft-js-buttons";
+import "./editorStyles.css";
+import "draft-js-static-toolbar-plugin/lib/plugin.css";
+
+class HeadlinesPicker extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      window.addEventListener("click", this.onWindowClick);
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("click", this.onWindowClick);
+  }
+
+  onWindowClick = () =>
+    // Call `onOverrideContent` again with `undefined`
+    // so the toolbar can show its regular content again.
+    this.props.onOverrideContent(undefined);
+
+  render() {
+    const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
+    return (
+      <div>
+        {buttons.map((
+          Button,
+          i // eslint-disable-next-line
+        ) => (
+          <Button key={i} {...this.props} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class HeadlinesButton extends React.Component {
+  onClick = () =>
+    // A button can call `onOverrideContent` to replace the content
+    // of the toolbar. This can be useful for displaying sub
+    // menus or requesting additional information from the user.
+    this.props.onOverrideContent(HeadlinesPicker);
+
+  render() {
+    return (
+      <div className="headlineButtonWrapper">
+        <button onClick={this.onClick} className="headlineButton">
+          H
+        </button>
+      </div>
+    );
+  }
+}
+
+const toolbarPlugin = createToolbarPlugin();
+const { Toolbar } = toolbarPlugin;
+const plugins = [toolbarPlugin];
+
+export default class CustomTextEditor extends React.Component {
+  focus = () => {
+    this.editor.focus();
+  };
+
+  render() {
+    const { editorState, onEditorChange } = this.props;
+    return (
+      <Fragment>
+        <div className="editor" onClick={this.focus}>
+          <Editor
+            editorState={editorState}
+            onChange={onEditorChange}
+            plugins={plugins}
+            ref={element => {
+              this.editor = element;
+            }}
+          />
+          <Toolbar>
+            {// may be use React.Fragment instead of div to improve perfomance after React 16
+            externalProps => (
+              <Fragment>
+                <BoldButton {...externalProps} />
+                <ItalicButton {...externalProps} />
+                <UnderlineButton {...externalProps} />
+                <CodeButton {...externalProps} />
+                <Separator {...externalProps} />
+                <HeadlinesButton {...externalProps} />
+                <UnorderedListButton {...externalProps} />
+                <OrderedListButton {...externalProps} />
+                <BlockquoteButton {...externalProps} />
+                <CodeBlockButton {...externalProps} />
+              </Fragment>
+            )}
+          </Toolbar>
+        </div>
+      </Fragment>
+    );
+  }
+}
